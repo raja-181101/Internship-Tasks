ae0a63778b97403181b860dbdb42edb40ff01ca1
Rajapandu_M <rajapandu2024@gmail.com>
2026-08-26 23:17:05 +0530

    styling completed for home page next to grgistration form

--- Task3/Server.js
+++ Task3/Server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 const users = [];
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("css"));
+app.use(express.static(path.join(__dirname,"public")));
 app.get('/',(req,res)=>{
     res.render('index');
 });
